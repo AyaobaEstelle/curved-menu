015a2fa448736717abfb454ac372e516b4fd90b3
AyaobaEstelle <abolarinesther98@gmail.com>
2023-11-07 14:46:20 +0100

    work in progress

---
+++ src/components/header/index.jsx
@@ -0,0 +1,18 @@
+'use client'
+import styles from './style.module.scss'
+import { useState } from 'react';
+
+function Home() {
+
+  const [isActive, setIsActive] = useState(false);
+
+  return (
+    <>
+    <div onClick={() => {setIsActive(!isActive)}} className={styles.button}>
+      <div className={`${styles.burger} ${isActive ? styles.burgerActive : ""}`}></div>
+    </div>
+    </>
+  )
+}
+
+export default Home;
